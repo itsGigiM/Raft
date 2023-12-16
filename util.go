36cf90d954366c872ff39da02521a1a5838bbd02
itsGigiM <gmirz20@freeuni.edu.ge>
2023-12-16 21:42:50 +0400

    Raft added

---
+++ util.go
@@ -0,0 +1,14 @@
+package raft
+
+import "log"
+
+// Debugging
+const Debug = false
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+		log.Printf(format, a...)
+	}
+	return
+}
